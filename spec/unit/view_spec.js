17f5036a6ab3f0d92504be6ff813de911aa2cd7e
Barbara Mucha <basiamucha@gmail.com>
2013-09-16 10:24:27 -0400

    Updating tracker.js

--- spec/unit/view_spec.js
+++ spec/unit/view_spec.js
@@ -6,26 +6,6 @@ describe 'View'
       var view = new v.View(env)
       view.urlKey().should.equal "gucci"
     end
-
-    it 'extracts a url_key from a city offer url'
-      var env = { u: "http://newyork.gilt.com/city/offer/rougetomate" }
-      var view = new v.View(env)
-      view.urlKey().should.equal "rougetomate"
-    end
-
-    it 'extracts a url_key from a product url'
-      var env = { u: "http://localhost/s/gucci/product/12345" }
-
-      var view = new v.View(env)
-      view.urlKey().should.equal "gucci"
-    end
-
-    it 'handles bad urls gracefully'
-      var env = { u: "asdfasfasd/sdfof/foo" }
-
-      var view = new v.View(env)
-      view.urlKey().should.be_null
-    end
   end
 
   describe '.productId()'
@@ -34,19 +14,6 @@ describe 'View'
 
       var view = new v.View(env)
       view.productId().should.equal "12345"
-    end
-
-    it 'handles other stuff at the end of the url'
-      var env = { u: "http://localhost/s/gucci/product/12345?foobar" }
-
-      var view = new v.View(env)
-      view.productId().should.equal "12345"
-    end
-
-    it 'should not extract a productId from a city offer url'
-      var env = { u: "http://newyork.gilt.com/city/offer/rougetomate" }
-      var view = new v.View(env)
-      isNaN(view.productId()).should.be_true
     end
   end
 
